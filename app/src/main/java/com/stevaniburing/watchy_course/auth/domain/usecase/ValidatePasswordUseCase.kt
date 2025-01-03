9e79d636903bc81edd6e5a3b630864e0f38aca08
stevani03 <147477260+stevani03@users.noreply.github.com>
2025-01-03 23:31:03 +0700

    Update ValidatePasswordUseCase.kt

--- app/src/main/java/com/stevaniburing/watchy_course/auth/domain/usecase/ValidatePasswordUseCase.kt
+++ app/src/main/java/com/stevaniburing/watchy_course/auth/domain/usecase/ValidatePasswordUseCase.kt
@@ -1,34 +1,17 @@
 package com.stevaniburing.watchy_course.auth.domain.usecase
 
-/**
- * @author Ahmed Guedmioui
- */
+// Kelas untuk memvalidasi password
 class ValidatePasswordUseCase {
 
+    // Operator function yang akan dipanggil untuk memvalidasi password
     operator fun invoke(password: String): Boolean {
+        // Memeriksa apakah password memenuhi kriteria:
+        // 1. Panjang password minimal 8 karakter
+        // 2. Password mengandung huruf kapital
+        // 3. Password mengandung angka
         return password.length >= 8 &&
-                password.any { it.isUpperCase() } &&
-                password.any { it.isDigit() }
+                password.any { it.isUpperCase() } && // Memeriksa adanya huruf kapital
+                password.any { it.isDigit() } // Memeriksa adanya angka
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
