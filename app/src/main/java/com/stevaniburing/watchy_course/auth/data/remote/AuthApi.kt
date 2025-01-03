64d1629882c3af758f183dda13c89d18291c8de8
stevani03 <147477260+stevani03@users.noreply.github.com>
2025-01-03 23:23:29 +0700

    Update AuthApi.kt

--- app/src/main/java/com/stevaniburing/watchy_course/auth/data/remote/AuthApi.kt
+++ app/src/main/java/com/stevaniburing/watchy_course/auth/data/remote/AuthApi.kt
@@ -1,47 +1,39 @@
 package com.stevaniburing.watchy_course.auth.data.remote
 
+// Mengimpor DTO yang digunakan untuk permintaan dan respons otentikasi
 import com.stevaniburing.watchy_course.auth.data.remote.dto.AuthRequest
 import com.stevaniburing.watchy_course.auth.data.remote.dto.AuthRespond
+
+// Mengimpor konstanta yang digunakan untuk URL backend
 import com.stevaniburing.watchy_course.util.BackendConstants
+
+// Mengimpor Retrofit2 untuk menangani HTTP requests
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
 
-/**
- * @author Ahmed Guedmioui
- */
+// Interface untuk mendefinisikan API otentikasi dengan Retrofit
 interface AuthApi {
 
+    // Endpoint untuk pendaftaran pengguna baru
     @POST(BackendConstants.REGISTER)
     suspend fun register(
+        // Menyertakan permintaan otentikasi yang berisi data pengguna
         @Body authRequest: AuthRequest
     )
 
+    // Endpoint untuk login pengguna
     @POST(BackendConstants.LOGIN)
     suspend fun login(
+        // Menyertakan permintaan otentikasi yang berisi data pengguna (email dan password)
         @Body authRequest: AuthRequest
     ): AuthRespond
 
+    // Endpoint untuk memverifikasi token otentikasi pengguna
     @GET(BackendConstants.AUTHENTICATE)
     suspend fun authenticate(
+        // Menyertakan token otentikasi di header Authorization
         @Header("Authorization") token: String
     )
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
