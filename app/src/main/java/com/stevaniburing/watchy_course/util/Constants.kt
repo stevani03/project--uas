8c8c7fb8d97680c7f056bbee0dde3c519c9b2915
stevani03 <147477260+stevani03@users.noreply.github.com>
2025-01-03 23:34:58 +0700

    Update Constants.kt

--- app/src/main/java/com/stevaniburing/watchy_course/util/Constants.kt
+++ app/src/main/java/com/stevaniburing/watchy_course/util/Constants.kt
@@ -2,7 +2,9 @@ package com.stevaniburing.watchy_course.util
 
 import com.stevaniburing.watchy_course.main.domain.model.Genre
 
+// Kelas Constants yang menyimpan beberapa konstanta yang digunakan di aplikasi
 object Constants {
+    // Nama-nama kategori untuk media yang akan ditampilkan
     const val actionAndAdventureList = "Action And Adventure"
     const val dramaList = "Drama"
     const val comedyList = "Comedy"
@@ -10,25 +12,29 @@ object Constants {
     const val animationList = "Animation"
 }
 
+// Kelas BackendConstants yang menyimpan URL dasar dan endpoint-endpoint untuk API backend
 object BackendConstants {
 
+    // URL dasar untuk API backend
     const val BACKEND_BASE_URL = "http://192.168.10.162:8081/"
 
+    // Endpoint untuk berbagai operasi autentikasi
     const val REGISTER = "register"
     const val LOGIN = "login"
     const val AUTHENTICATE = "authenticate"
 
+    // Endpoint untuk mengambil dan memanipulasi data media yang disukai dan dibookmark
     const val GET_LIKED_MEDIA_LIST = "get-liked-media-list/{email}"
     const val GET_BOOKMARKED_MEDIA_LIST = "get-bookmarked-media-list/{email}"
     const val GET_MEDIA_BY_ID = "get-media-by-id/{mediaId}/{email}"
     const val UPSERT_MEDIA_TO_USER = "upsert-media-to-user"
     const val DELETE_MEDIA_FROM_USER = "delete-media-from-user"
-
 }
 
+// Kelas APIConstants yang menyimpan query dan informasi terkait API dari TMDB
 object APIConstants {
 
-    // Queries for API calls
+    // Konstanta untuk query yang digunakan pada pemanggilan API
     const val POPULAR = "popular"
     const val TRENDING = "trending"
     const val TRENDING_TIME = "day"
@@ -36,10 +42,10 @@ object APIConstants {
     const val MOVIE = "movie"
     const val TV = "tv"
 
-
-    // Tag for debugging
+    // Tag untuk debugging API requests
     const val GET_TAG = "--> GET https://api.themoviedb.org"
 
+    // Daftar genre yang digunakan untuk mengelompokkan media
     val genres = listOf(
         Genre(12, "Adventure"),
         Genre(14, "Fantasy"),
@@ -69,20 +75,4 @@ object APIConstants {
         Genre(10768, "War & Politics"),
         Genre(10770, "TV Movie")
     )
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
