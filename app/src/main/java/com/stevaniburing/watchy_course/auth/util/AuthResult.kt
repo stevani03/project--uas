c0faf879aa26db7c2ef99d56011dd3f6704e7f8d
stevani03 <147477260+stevani03@users.noreply.github.com>
2025-01-03 23:31:52 +0700

    Update AuthResult.kt

--- app/src/main/java/com/stevaniburing/watchy_course/auth/util/AuthResult.kt
+++ app/src/main/java/com/stevaniburing/watchy_course/auth/util/AuthResult.kt
@@ -1,11 +1,19 @@
 package com.stevaniburing.watchy_course.auth.util
 
+// Sealed class untuk menangani hasil otentikasi
 sealed class AuthResult<T>(
-    val data: T? = null,
-    val message: String? = null
+    val data: T? = null, // Data terkait hasil otentikasi (optional)
+    val message: String? = null // Pesan kesalahan (optional)
 ) {
+    // Hasil otentikasi berhasil, dengan data (jika ada)
     class Authorized<T>(data: T? = null) : AuthResult<T>(data)
+
+    // Hasil otentikasi gagal karena kredensial tidak sah
     class Unauthorized<T> : AuthResult<T>()
+
+    // Hasil otentikasi gagal karena kesalahan yang tidak diketahui
     class UnknownError<T> : AuthResult<T>()
+
+    // Hasil otentikasi gagal karena pengguna telah keluar
     class SingedOut<T> : AuthResult<T>()
 }
