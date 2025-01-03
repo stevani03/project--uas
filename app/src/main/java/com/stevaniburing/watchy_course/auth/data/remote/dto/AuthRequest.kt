06c9bc971f26844611e039d0c5a7e18a0b0e9f7e
stevani03 <147477260+stevani03@users.noreply.github.com>
2025-01-03 23:22:03 +0700

    Update AuthRequest.kt

--- app/src/main/java/com/stevaniburing/watchy_course/auth/data/remote/dto/AuthRequest.kt
+++ app/src/main/java/com/stevaniburing/watchy_course/auth/data/remote/dto/AuthRequest.kt
@@ -1,7 +1,13 @@
 package com.stevaniburing.watchy_course.auth.data.remote.dto
 
+// Data class ini mendefinisikan struktur data untuk permintaan otentikasi (AuthRequest)
 data class AuthRequest(
-    val name: String = "",
-    val email: String,
+    // Nama pengguna yang dikirimkan dalam permintaan otentikasi, dengan nilai default sebagai string kosong
+    val name: String = "", 
+    
+    // Alamat email pengguna yang diperlukan untuk otentikasi
+    val email: String, 
+    
+    // Kata sandi pengguna yang digunakan untuk otentikasi, dengan nilai default sebagai string kosong
     val password: String = ""
 )
