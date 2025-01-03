baae9787178dc872dad7963ed57c0c74743dbca4
stevani03 <147477260+stevani03@users.noreply.github.com>
2025-01-03 23:22:42 +0700

    Update AuthRespond.kt

--- app/src/main/java/com/stevaniburing/watchy_course/auth/data/remote/dto/AuthRespond.kt
+++ app/src/main/java/com/stevaniburing/watchy_course/auth/data/remote/dto/AuthRespond.kt
@@ -1,6 +1,10 @@
 package com.stevaniburing.watchy_course.auth.data.remote.dto
 
+// Data class ini mendefinisikan struktur data untuk respons otentikasi (AuthRespond)
 data class AuthRespond(
-    val name: String,
+    // Nama pengguna yang diterima sebagai bagian dari respons otentikasi
+    val name: String, 
+    
+    // Token otentikasi yang diberikan setelah pengguna berhasil melakukan otentikasi
     val token: String
 )
