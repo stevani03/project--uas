1fa37e1e51cc4e260f9fcccef39b825d8572cd2d
stevani03 <147477260+stevani03@users.noreply.github.com>
2025-01-03 23:29:57 +0700

    Update ValidateNameUseCase.kt

--- app/src/main/java/com/stevaniburing/watchy_course/auth/domain/usecase/ValidateNameUseCase.kt
+++ app/src/main/java/com/stevaniburing/watchy_course/auth/domain/usecase/ValidateNameUseCase.kt
@@ -1,11 +1,11 @@
 package com.stevaniburing.watchy_course.auth.domain.usecase
 
-/**
- * @author Ahmed Guedmioui
- */
+// Kelas untuk memvalidasi panjang nama
 class ValidateNameUseCase {
 
+    // Operator function yang akan dipanggil untuk memvalidasi nama
     operator fun invoke(name: String): Boolean {
+        // Memeriksa apakah panjang nama berada dalam rentang 4 hingga 50 karakter
         return name.length in 4..50
     }
 
