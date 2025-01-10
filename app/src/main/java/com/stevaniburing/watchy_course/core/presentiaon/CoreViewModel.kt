9bbb8dcd3c600e1d020a261a1c56becee9947c79
stevani03 <147477260+stevani03@users.noreply.github.com>
2025-01-10 08:22:10 +0700

    Update CoreViewModel.kt

--- app/src/main/java/com/stevaniburing/watchy_course/core/presentiaon/CoreViewModel.kt
+++ app/src/main/java/com/stevaniburing/watchy_course/core/presentiaon/CoreViewModel.kt
@@ -1,4 +1,4 @@
-package com.stevaniburing.watchy_course.core.presentiaon
+package com.stevaniburing.watchy_course.core.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -14,50 +14,41 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
+ // ViewModel untuk modul inti aplikasi, bertanggung jawab untuk autentikasi pengguna.
+ 
+ // @param authRepository Repository autentikasi yang digunakan untuk operasi login atau autentikasi.
+ 
 @HiltViewModel
 class CoreViewModel @Inject constructor(
     private val authRepository: AuthRepository
-): ViewModel() {
+) : ViewModel() {
 
+    // Channel untuk mengirim hasil autentikasi secara asinkron.
     private val _authResultChannel = Channel<AuthResult<Unit>>()
-    val authResultChannel = _authResultChannel.receiveAsFlow()
+    val authResultChannel = _authResultChannel.receiveAsFlow() // Ekspos channel sebagai Flow.
 
+    // StateFlow untuk menunjukkan status loading.
     private val _isLoading = MutableStateFlow(true)
-    val isLoading = _isLoading.asStateFlow()
+    val isLoading = _isLoading.asStateFlow() // Ekspos StateFlow sebagai read-only.
 
+    // Blok inisialisasi untuk memulai proses autentikasi saat ViewModel dibuat.
     init {
         authenticate()
     }
 
+    
+     // Fungsi untuk menjalankan autentikasi pengguna.
+     // Menggunakan viewModelScope untuk menjalankan operasi di dalam coroutine.
+     // Mengatur state loading ke `true` sebelum memulai autentikasi.
+     // Memanggil metode autentikasi dari `authRepository` dan mengirimkan hasilnya ke `_authResultChannel`.
+     // Setelah selesai, mengatur state loading ke `false`.
     private fun authenticate() {
         viewModelScope.launch {
-            _isLoading.update { true }
-            val result = authRepository.authenticate()
-            _authResultChannel.send(result)
-            _isLoading.update { false }
+            _isLoading.update { true } // Mengatur status loading ke `true`.
+            val result = authRepository.authenticate() // Memanggil autentikasi dari repository.
+            _authResultChannel.send(result) // Mengirim hasil autentikasi melalui channel.
+            _isLoading.update { false } // Mengatur status loading ke `false`.
         }
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
